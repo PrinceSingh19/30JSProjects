a466e4c5ca95052513f1390416dd86ec78190dfe
Prince Singh <princesingh808087@gmail.com>
2022-08-27 17:55:03 +0530

    Modified the Day02JsClock and added some new functionalities

--- Day02JSClock/script.js
+++ Day02JSClock/script.js
@@ -1,20 +1,23 @@
-  const secondHand = document.querySelector('.second-hand');
-  const minsHand = document.querySelector('.min-hand');
-  const hourHand = document.querySelector('.hour-hand');
- function setDate() {
-    const now = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+"use strict";
+(function(){
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+function setDate(){
+  const rightNow = new Date();
+  const seconds = rightNow.getSeconds();
+  const minutes = rightNow.getMinutes();
+  const hours = rightNow.getHours();
+  
+  const secondDegrees= ((seconds/60)*360)+90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const minuteDegrees = ((minutes/60)*360)+90;
+  minuteHand.style.transform= `rotate(${minuteDegrees}deg)`;
 
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-  }
-  setInterval(setDate, 1000);
-  setDate();
+  const hourDegrees = ((hours*360)/24);
+  hourHand.style.transform= `rotate(${hourDegrees}deg)`;
+}
+setInterval(setDate,1000);
+})()
